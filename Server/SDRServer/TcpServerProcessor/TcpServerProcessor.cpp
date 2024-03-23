40083abd7da0092206c74865fd3f754fb153db1d
petr <petr.chizhikov@bk.ru>
2024-03-23 23:49:18 +0300

    Tcp сервер. Начальная настройка.

--- Server/SDRServer/TcpServerProcessor/TcpServerProcessor.cpp
+++ Server/SDRServer/TcpServerProcessor/TcpServerProcessor.cpp
@@ -1,6 +1,61 @@
 #include "TcpServerProcessor.h"
 
-TcpServerProcessor::TcpServerProcessor()
+TcpServerProcessor::TcpServerProcessor(QObject *parent)
 {
-    qDebug() << "TcpServerProcessor";
+    tcpServer = std::make_unique<QTcpServer>();
+    if(tcpServer->listen(QHostAddress::Any, portForListen))
+    {
+        qDebug() << "TcpServerProcessor started on port " << portForListen << "\n";
+        connect(tcpServer.get(), &QTcpServer::newConnection,
+                this, &TcpServerProcessor::newConnection);
+    }else {
+        qDebug() << "TcpServerProcessor not started";
+    }
+}
+
+void TcpServerProcessor::SendToClient(QByteArray data)
+{
+    if (sockets.empty()) return;
+    for (auto socket : sockets)
+    {
+        socket->write(data);
+        socket->flush();
+    }
+
+}
+
+void TcpServerProcessor::newConnection()
+{
+    //Возвращается объект клиента?
+    auto newSocket = std::shared_ptr<QTcpSocket>(tcpServer->nextPendingConnection());
+    qDebug() << "tcp client connected: " << newSocket->peerAddress().toString() << "on port: " << QString::number(newSocket->peerPort());
+
+    connect(newSocket.get(), &QTcpSocket::readyRead, this, &TcpServerProcessor::processMessage, Qt::QueuedConnection);
+    connect(newSocket.get(), &QTcpSocket::disconnected, this, &TcpServerProcessor::socketDisconnected, Qt::QueuedConnection);
+
+    sockets.push_back(newSocket);
+
+    //message after Connect
+    QString clientConnectedStatus = "Client connected";
+    newSocket->write(QByteArray::fromStdString(clientConnectedStatus.toStdString()));
+    newSocket->flush();
+}
+
+void TcpServerProcessor::socketDisconnected()
+{
+    auto senderClient = std::dynamic_pointer_cast<QTcpSocket>(std::shared_ptr<QObject>(sender(),[](QObject* obj){}));
+    if(senderClient)
+    {
+        disconnect(senderClient.get(), &QTcpSocket::readyRead, this, &TcpServerProcessor::processMessage);
+        disconnect(senderClient.get(), &QTcpSocket::disconnected, this, &TcpServerProcessor::socketDisconnected);
+        sockets.remove(senderClient);
+    }
+}
+
+void TcpServerProcessor::processMessage()
+{
+    auto senderClient = std::dynamic_pointer_cast<QTcpSocket>(std::shared_ptr<QObject>(sender(),[](QObject* obj){}));
+    QByteArray data = senderClient->readAll();
+    qDebug() << "Your message: " <<  QString::fromUtf8(data);
+    SendToClient(data);
 }
--- Server/SDRServer/TcpServerProcessor/TcpServerProcessor.h
+++ Server/SDRServer/TcpServerProcessor/TcpServerProcessor.h
@@ -1,9 +1,32 @@
 #pragma once
 #include <QDebug>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <vector>
 
-class TcpServerProcessor
+class TcpServerProcessor : public QTcpServer
 {
+    Q_OBJECT
 public:
-    TcpServerProcessor();
+    TcpServerProcessor(QObject *parent = 0);
+
+
+private:
+    std::list<std::shared_ptr<QTcpSocket>> sockets;
+    QByteArray Data;
+    int16_t portForListen = 2323;
+    std::unique_ptr<QTcpServer> tcpServer;
+
+
+    void SendToClient(QByteArray data);
+
+
+public slots:
+//    qintptr Этот тип гарантирует, что будет тем же размером как указатель на всех платформах, поддерживаемых QT.
+//    В системе с 32-разрядными указателями qintptr является определением типа для qint32;
+//    в системе с 64-разрядными указателями qintptr является определением типа для qint64.
+    void newConnection();
+    void processMessage();
+    void socketDisconnected();
 };
 
